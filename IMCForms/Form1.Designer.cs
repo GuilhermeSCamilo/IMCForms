a5110c57ed37becb4141edb1d5f7c4e922bc6783
Guilherme Santos Camilo <guilhermescamiloo@gmail.com>
2023-10-30 19:34:36 -0300

    Initial commit

---
+++ IMCForms/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace IMCForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txbImc = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(114, 94);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(100, 20);
+            this.txbPeso.TabIndex = 0;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(114, 133);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 1;
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(64, 101);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 2;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(64, 140);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblImc
+            // 
+            this.lblImc.AutoSize = true;
+            this.lblImc.Location = new System.Drawing.Point(67, 233);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(26, 13);
+            this.lblImc.TabIndex = 4;
+            this.lblImc.Text = "IMC";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(67, 182);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(147, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txbImc
+            // 
+            this.txbImc.Enabled = false;
+            this.txbImc.Location = new System.Drawing.Point(114, 233);
+            this.txbImc.Name = "txbImc";
+            this.txbImc.Size = new System.Drawing.Size(100, 20);
+            this.txbImc.TabIndex = 6;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(70, 278);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 303);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txbImc);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblImc);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbPeso);
+            this.Name = "Form1";
+            this.Text = "Cálculo do IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblImc;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txbImc;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ IMCForms/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMCForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declarar Variável:
+            double peso, altura, imc;
+
+            try
+            {
+                //Declarar valor da variável:
+                altura = double.Parse(txbAltura.Text);
+                peso = double.Parse(txbPeso.Text);
+
+                //Efetuar a operação:
+                txbImc.Text = ((peso / (altura * altura)).ToString());
+                imc = double.Parse(txbImc.Text);
+
+                if (imc <= 18.5)
+                {
+                    lblStatus.Text = "Abaixo do peso";
+
+                }
+                else if (imc >= 18.5 && imc <= 24.9)
+                {
+                    lblStatus.Text = "Peso Ideal (Parabéns!!)";
+
+                }
+                else if (imc >= 25.0 && imc <= 29.9)
+                {
+                    lblStatus.Text = "Levemente acima do peso";
+
+                }
+                else if (imc >= 30.0 && imc <= 34.9)
+                {
+                    lblStatus.Text = "Obesidade grau I";
+
+                }
+                else if (imc >= 35.0 && imc <= 39.9)
+                {
+                    lblStatus.Text = "Obesidade grau II(Severa)";
+
+                }
+                else if (imc >= 40.0)
+                {
+                    lblStatus.Text = "Obesidade grau III(Mórbida)";
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Dados Invalidos Informados!!!");
+                //Limpar txb's
+                txbAltura.Clear();
+                txbPeso.Clear();
+                txbImc.Clear();
+            }
+
+        }
+    }
+}
